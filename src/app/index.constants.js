a61d42e62f5d13a2cb8ab0b134e0064a12f3a30f
wangbin62 <wangbin_62@126.com>
2015-12-27 14:55:24 +0800

    init

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('zeyogen')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
