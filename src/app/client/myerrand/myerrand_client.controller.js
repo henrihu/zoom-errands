67c1321a70e86cb9a9fdbe809ada08c8793b5dfa
wangbin <wangbin_62@yahoo.com>
2016-01-08 17:19:55 +0800

    change myerrand

--- src/app/client/myerrand/myerrand_client.controller.js
+++ src/app/client/myerrand/myerrand_client.controller.js
@@ -7,15 +7,30 @@
         .controller('MyerrandClientController', MyerrandClientController);
 
     /** @ngInject */
-    MyerrandClientController.$inject = ['$log', '$scope', 'Restangular'];
-    function MyerrandClientController($log, $scope, Restangular)
+    MyerrandClientController.$inject = ['$log', '$scope', 'Restangular', 'toastr'];
+    function MyerrandClientController($log, $scope, Restangular, toastr)
     {
         var vm = this; 
+        vm.deleteTask = deleteTask;
+
         Restangular.all('client/tasks').getList()
         .then(function(tasks) {
             vm.tasks = tasks;
             vm.displayedtasks = [].concat(vm.tasks);
             $log.log(vm.displayedtasks);
         });
+
+        function deleteTask(taskid) 
+        {   
+            Restangular.one('client/tasks', taskid).remove()
+            .then(function(data) {
+                // vm.tasks.slice(data.id);
+                toastr.success('Your task ' + data.title + 'has been cancelled.');
+            }, function(error) {
+                toastr.error(error);
+            });
+
+        }
+
     }
 })();
