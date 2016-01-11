dd77fd0ae55eed94fde899c34aca5e4f21dfb2c9
akopichin <akopichin@gmail.com>
2016-01-11 13:11:46 +0300

    seems ready

--- src/app/client/timestatus/timestatus_client.controller.js
+++ src/app/client/timestatus/timestatus_client.controller.js
@@ -11,7 +11,20 @@
     function TimestatusClientController($log, $scope, Restangular, toastr)
     {
         var vm = this;
-        console.log('Timestatus controller');
+
+        // temp vals
+        vm.eh = {
+            hoursavail: 33,
+            hoursused: -56,
+            escrowavail:	265.92,
+            escrowused: -47.89
+        };
+
+        Restangular.one('client/escrowhours').get()
+        .then(function(data) {
+            vm.eh = data.eh ? data.eh : vm.eh;
+            $log.log(data);
+        });
 
     }
 })();
