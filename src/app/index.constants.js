9fa449caf0bbe0a5d364ff3b45547711e0e57d02
wangbin <wangbin_62@yahoo.com>
2016-02-05 11:27:58 +0800

    as

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -4,12 +4,12 @@
   angular
     .module('zeyogen')
     // .constant('moment', moment);
-    .constant('API_URL', 'http://localhost:3000/api/v1')
-    .constant('HOST_URL', 'http://localhost:3000');
+    // .constant('API_URL', 'http://localhost:3000/api/v1')
+    // .constant('HOST_URL', 'http://localhost:3000');
     // .constant('API_URL', 'http://192.168.10.111:3000/api/v1')
     // .constant('HOST_URL', 'http://192.168.10.111:3000');
-    // .constant('API_URL', 'http://52.8.215.245:3000/api/v1')
-    // .constant('HOST_URL', 'http://52.8.215.245:3000');
+    .constant('API_URL', 'http://52.8.215.245:3000/api/v1')
+    .constant('HOST_URL', 'http://52.8.215.245:3000');
 
 
 })();
