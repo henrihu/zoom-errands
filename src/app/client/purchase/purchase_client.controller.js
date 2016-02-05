46f00e48965b27ec3d4c33d63ddaa797517aa7fb
wangbin <wangbin_62@yahoo.com>
2016-02-05 09:54:24 +0800

    add coupon function

--- src/app/client/purchase/purchase_client.controller.js
+++ src/app/client/purchase/purchase_client.controller.js
@@ -10,7 +10,19 @@
     PurchaseClientController.$inject = ['$log','$window', '$scope', 'Restangular', 'toastr'];
     function PurchaseClientController($log, $window, $scope, Restangular, toastr)
     {
-        var vm = this;
+        var vm = this;       
+
+        vm.hour = 2;
+        vm.cost = 35;
+        vm.escrow = 30;
+        vm.fee = {percent: 0, cent: 0};
+        vm.couponPercent = 0;
+        vm.coupon = 0;
+
+        Restangular.one('client/escrowhours/fee').get()
+        .then(function(data) {
+            vm.fee = data.fee;
+        });
 
         // temp vals
         vm.eh = {
@@ -28,12 +40,42 @@
             toastr.warning(data.data.alert);
         });
 
+        $scope.$watch('vm.escrow', function() {
+            vm.subtotal = vm.escrow + vm.hour * vm.cost;
+        });
+
+        $scope.$watch('vm.subtotal', function() {
+            vm.proFee = vm.subtotal*vm.fee.percent*0.01 + vm.fee.cent*0.01;
+            vm.total = (vm.subtotal + vm.proFee) * (1 - vm.couponPercent*0.01);
+        });
+
+        vm.couponApply = function() {
+            if (vm.tmpcoupon) {
+                Restangular.one('client/escrowhours/coupon_check').get({'couponCode': vm.tmpcoupon})
+                .then(function(resp) {
+                    vm.couponPercent = resp.percent;
+                    vm.total = (vm.subtotal + vm.proFee) * (1 - vm.couponPercent*0.01); 
+                }, function(resp) {
+                    vm.couponPercent = 0;
+                    vm.total = (vm.subtotal + vm.proFee) * (1 - vm.couponPercent*0.01); 
+                    toastr.warning(resp.data.error);
+                });
+            } else {
+                vm.couponPercent = 0;
+                vm.total = (vm.subtotal + vm.proFee) * (1 - vm.couponPercent*0.01); 
+                toastr.warning("Please input Coupon code correctly.");
+            }
+            // vm.total = (vm.subtotal + vm.proFee) * (1 - vm.couponPercent*0.01);  
+            vm.coupon = vm.tmpcoupon;                      
+        };
+
         // Stripe Response Handler
         $scope.stripeCallback = function (code, result) {
             if (result.error) {
                 $window.alert('it failed! error: ' + result.error.message);
             } else {                
-                var payload = {stripeEmail: $scope.user.email, stripeToken: result.id};
+                var payload = {stripeEmail: $scope.user.email, stripeToken: result.id, purchaseHour: vm.hour,
+                             purchaseEscrow: vm.escrow, couponCode: vm.coupon};
                 Restangular.all('client/escrowhours/charge').post(payload)
                 .then(function(resp) {
                     $log.log(resp);
