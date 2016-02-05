339a0b684408771a66cf46b88069ae6a1efa9fc1
akopichin <akopichin@gmail.com>
2016-02-05 05:39:59 +0300

    arrows

--- src/app/client/dashboard/dashboard_client.controller.js
+++ src/app/client/dashboard/dashboard_client.controller.js
@@ -159,6 +159,24 @@
 
         }
 
+        vm.breakpointCards = [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 2,
+                    arrows: false
+                }
+            }, {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    arrows: false
+                }
+            }
+        ];
+
         /*
         function loadMore()
         {
