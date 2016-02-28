c648195aca9a0ca549011ac9a8a079fec676a4f3
akopichin <akopichin@gmail.com>
2016-02-28 21:45:10 +0300

    js work. >> test pls

--- src/app/client/purchase/purchase_client.controller.js
+++ src/app/client/purchase/purchase_client.controller.js
@@ -12,12 +12,15 @@
     {
         var vm = this;       
 
-        vm.hour = 2;
+        vm.hour = 1;
         vm.cost = 35;
         vm.escrow = 0;
         vm.fee = {percent: 0, cent: 0};        
         vm.couponPercent = 0;
         vm.coupon = 0;
+        vm.showDropdown = false;
+        vm.dropBtnText = '1 hour';
+        vm.hoursPrice = 32;
 
         Restangular.one('client/escrowhours/fee').get()
         .then(function(data) {
@@ -42,6 +45,10 @@
             toastr.warning(data.data.alert);
         });
 
+        $scope.$watch('vm.hour', function() {
+            vm.subtotal = vm.escrow + vm.hour * vm.cost;
+        });
+
         $scope.$watch('vm.escrow', function() {
             vm.subtotal = vm.escrow + vm.hour * vm.cost;
         });
@@ -53,6 +60,15 @@
 
         vm.couponCalcel = function() {
             // What is here?
+        }
+
+        vm.setHours = function(h) {
+            vm.hour = h;
+            if (h < 5){
+                vm.dropBtnText = h + ' ' + (h == 1 ? 'hour' : 'hours');
+                vm.hoursPrice = h * 32;
+                vm.showDropdown = false;
+            }
         }
 
         vm.couponApply = function() {
