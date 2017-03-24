cea333f48401a2197f647287c18174d8e7df1d3d
henri <henrih1@zoho.com>
2017-03-24 22:27:28 +0800

    git ignore log

--- config/deploy.rb
+++ config/deploy.rb
@@ -59,7 +59,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      nginx:restart
+      invoke 'nginx:restart'
     end
   end
 
