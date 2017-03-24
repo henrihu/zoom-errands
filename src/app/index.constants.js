5cecd76c89ec0cba61eb4d995c7751c82d2cd45e
henri <henrih1@zoho.com>
2017-03-24 22:21:44 +0800

    fix api_url

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -8,7 +8,7 @@
     // .constant('HOST_URL', 'http://localhost:3000');
     // .constant('API_URL', 'http://192.168.10.101:3000/api/v1')
     // .constant('HOST_URL', 'http://192.168.10.101:3000');
-    .constant('API_URL', 'https://api.zoomerrands.com/api/v1');
+    .constant('API_URL', 'http://api.zoomerrands.com/api/v1');
 
 
 })();
