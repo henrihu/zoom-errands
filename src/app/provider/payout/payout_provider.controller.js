8c899e0824df6ad6f77d50476def9fd4480e6b32
abhinavong <rordev@ongraph.com>
2017-11-27 18:59:27 +0530

    add api method for get payout in payout_controller.js

--- src/app/provider/payout/payout_provider.controller.js
+++ src/app/provider/payout/payout_provider.controller.js
@@ -10,6 +10,23 @@
     PayoutProviderController.$inject = ['$state', '$log', 'API_URL', '$scope', 'Restangular', '$stateParams', 'toastr', 'FileUploader', '$auth'];
     function PayoutProviderController($state, $log, API_URL, $scope, Restangular, $stateParams, toastr, FileUploader, $auth)
     {
+      var vm = this;
+      vm.loadMore = loadMore;
+      vm.limit = 7;
+      vm.curPos = 0;
+
+      Restangular.one('provider/tasks/get_pay_out').get({'limit': vm.limit, 'offset': vm.curPos})
+      .then(function(resp) {
+
+      });
+
+      function loadMore()
+      {
+          Restangular.one('provider/tasks/get_pay_out').get({'limit': vm.limit, 'offset': vm.curPos})
+          .then(function(resp) {
+              alert("hellosadasdasd")
+          });
+      }
 
     }
 })();
